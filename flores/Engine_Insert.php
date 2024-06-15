3a5e0b1e07d83f20f0f1ba5e30fc6810384316bf
karlota3 <160137973+karlota3@users.noreply.github.com>
2024-06-15 00:12:18 -0600

    Update Engine_Insert.php
    Formulario correcto

--- flores/Engine_Insert.php
+++ flores/Engine_Insert.php
@@ -1,8 +1,8 @@
- <?php
+<?php
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "conexion";
+$dbname = "flores";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -11,18 +11,26 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-//recoger los datos del form 
-$nombre =$_POST["Txt_Nombre"]; 
-$apellido = $_POST["Txt_Apellido"]; 
+// recoger los datos del form 
+$nombre = $_POST["Txt_Nombre"]; 
+$apellido_P = $_POST["Txt_Apellido_P"];
+$apellido_M = $_POST["Txt_Apellido_M"];
 $email = $_POST["Txt_email"]; 
+$sexo = $_POST["Sexo"]; // Missing semicolon added here
 
-//creamos la sentencia de insert
-
-$sql = "INSERT INTO cliente (firstname, lastname, email)
-VALUES ('$nombre', '$apellido', '$email')";
+// creamos la sentencia de insert
+$sql = "INSERT INTO cliente(CLIENTE_NOMBRE, CLIENTE_APE_PAT, CLIENTE_APE_MAT,
+CLIENTE_CORREO, SEXO)
+VALUES ('$nombre', '$apellido_P', '$apellido_M', '$email', '$sexo')";
 
 if ($conn->query($sql) === TRUE) {
   echo "Nuevo dato insertado ";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
